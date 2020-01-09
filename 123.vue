518a2c61963618c53c99791a76aa29507c319cfa
northpbear <1617682574@qq.com>
2020-01-09 21:42:43 +0800

    '测试2'

---
+++ 123.vue
@@ -0,0 +1,18 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "123",
+        data() {
+            return {
+                key: value
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
